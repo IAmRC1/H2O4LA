0442abfc3caad272c50e6bde6fd2206270ea4de0
IAmRC1 <rishabhcena1@gmail.com>
2018-12-04 17:16:32 +0530

    First Commmit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,14 +3,22 @@ var glide = new Glide('.glide', {
   startAt: 0,
   perView: 1,
   gap: 0,
-  autoplay: 3000,
+  autoplay: 2000,
   breakpoints: {
     1024: {
       perView: 1
     },
-    600: {
+    480: {
       perView: 1
     }
   }
 })
-glide.mount()
+glide.mount();
+
+var options = {
+  useEasing: true,
+  useGrouping: true,
+  separator: ',',
+};
+var numAnim = new CountUp('.sw_box', 0, 10000, 0, 2, options);
+numAnim.start();
